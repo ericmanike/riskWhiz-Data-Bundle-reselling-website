1ddbba15ff7493346b3015744900e4e9d8663a5f
Ericsson <manikeeric@gmail.com>
2026-05-11 15:57:15 +0000

    webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,42 +2,25 @@ import dbConnect from "@/lib/mongoose";
 import Order from "@/models/Order";
 import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic'
-
 export async function POST(request: Request) {
-
-    
-  try { 
+  try {
     await dbConnect();
-     const data = await request.json();
-    console.log("Data received from webhook : ", data)
- 
-    // const data = await request.json();
-    // console.log("Data received from webhook : ", data)
-    const { reference, status } = data;
-    
-    console.log('Reference:', reference);
-    console.log('Status:', status);
 
-    if (!reference || !status) {
-      return NextResponse.json({ error: "Missing transaction_id or status" }, { status: 400 });
-    }
+    const data = await request.json();
+
+    const { reference, status } = data;
+    console.log('Received webhook data:', data);
+
+    // if (!transaction_id || !status) {
+    //   return NextResponse.json({ error: "Missing transaction_id or status" }, { status: 400 });
+    // }
 
 
     const order = await Order.findOneAndUpdate(
-      { transaction_id: reference },
+      { transaction_id: reference,  },
       { status: status.toLowerCase() }
     );
-  
-    if(!order){
-      await Order.findOneAndUpdate(
-        { transaction_id: "S"+reference },
-        { status: status.toLowerCase() }
-      );
-    }
- 
-
-    console.log('order found', order)
+      console.log('order found', order)
     return NextResponse.json({ received: true });
 
   } catch (error) {
@@ -45,7 +28,6 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: "Webhook error" }, { status: 500 });
   }
 }
-
 
 
 /*
@@ -73,21 +55,7 @@ Received webhook data: {
 
 
 
-{
-    "id": 7988,
-    "type": "test_event",
-    "status": "DELIVERED",
-    "previous_status": "PROCESSING",
-    "order_code": "DKZ-TEST-RQ5WKR",
-    "reference": "REF-HETWWVUOTM",
-    "amount": 10,
-    "user_id": 4,
-    "occurred_at": "2026-04-10T21:15:44+00:00",
-    "test": true,
-    "metadata": {
-        "message": "This is a test webhook from Dakazina"
-    }
-}
+
 
 
 
